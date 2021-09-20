13a2e1958fe53da5fdcd62216286c1421d861bfa
Your Name <you@example.com>
2021-09-20 07:53:46 +0700

    bai-tap

---
+++ bai-tap/3-Lớp và đối tượng trong PHP/[Bài tập] Xây dựng lớp QuadraticEquation (Phương trình bậc hai)/QuadraticEquation.php
@@ -0,0 +1,47 @@
+<?php
+
+use JetBrains\PhpStorm\Pure;
+
+class QuadraticEquation
+{
+    public int $a;
+    public int $b;
+    public int $c;
+
+    public function __construct($a, $b, $c)
+    {
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
+    }
+
+    public function getA(): int
+    {
+        return $this->a;
+    }
+
+    public function getB(): int
+    {
+        return $this->b;
+    }
+
+    public function getC(): int
+    {
+        return $this->c;
+    }
+
+    #[Pure] public function getDiscriminant(): float|object|int
+    {
+        return pow($this->getB(), 2) - 4 * $this->getA() * $this->getC();
+    }
+
+    #[Pure] public function getRoot1(): float|int
+    {
+        return (-$this->getB() + sqrt($this->getDiscriminant())) / 2 * $this->getA();
+    }
+
+    #[Pure] public function getRoot2(): float|int
+    {
+        return (-$this->getB() - sqrt($this->getDiscriminant())) / 2 * $this->getA();
+    }
+}
